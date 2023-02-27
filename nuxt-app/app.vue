b9520de2dcdd24f2910fc4e35a9bf4418eadab3f
DESKTOP-KT6QP87\sam <sam86113@gmail.com>
2023-02-27 17:06:31 +0800

    about Tailwind CSS

--- nuxt-app/app.vue
+++ nuxt-app/app.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="bg-white py-24">
+    <div class="flex flex-col items-center">
+      <h1 class="text-6xl font-semibold text-gray-600">Hello, World!</h1>
+      <p class="mt-4 text-9xl font-bold text-gray-900">Nuxt3 app</p>
+    </div>
   </div>
 </template>
